e749a5b8676ce29783846ba641f1ea8688713bfc
Dvoryashin <al2020dv2020@gmail.com>
2023-07-04 11:24:50 +0500

    created the ability to store files in special folder

--- php/index.php
+++ php/index.php
@@ -1,5 +1,15 @@
 <?php
 
-print_r($_FILES['file-1']);
+$filename = $_FILES['uploadImage']['name'];
+
+$dirpath = dirname(getcwd());
+$location = $dirpath. '/files/' .$filename;
+
+echo $location;
+if (move_uploaded_file($_FILES['uploadImage']['tmp_name'], $location)) { 
+    echo '<p>The HTML5 and php file upload was a success!</p>'; 
+} else { 
+    echo '<p>The php and HTML5 file upload failed.</p>'; 
+}
 
 ?>
