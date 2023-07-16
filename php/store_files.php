fa588b9bb55e7b9afcdf06e19a36691c21da298e
Dvoryashin <al2020dv2020@gmail.com>
2023-07-16 16:54:03 +0500

    deleted show_images.php and all code from that files moved to store_files.php

--- php/store_files.php
+++ php/store_files.php
@@ -24,6 +24,29 @@ if (move_uploaded_file($_FILES['uploadImage']['tmp_name'], $location)) {
 
 }
 
+$mysqli = new mysqli('localhost', 'admin', 'password', 'saveimage');
+
+$result = $mysqli->query("SELECT username, filename FROM files;");
+
+$owns_and_files = array();
+
+while($row = $result->fetch_row()){
+
+    array_push($owns_and_files, $row);
+
+}
+
+$i = 0;
+$time = time() + (60 * 60 * 24 * 1);
+
+foreach($owns_and_files as $row){
+
+    echo implode('+', $row);
+    setcookie($i, implode('.', $row), $time, '/');
+    $i++;
+
+}
+
 setcookie('upload_result', $upload_result, $time, $path);
 
 header('Location: http://www.saveimage.com');
