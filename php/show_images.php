8f740785d39b109de47bec8f634ff8ce43bb149a
Dvoryashin <al2020dv2020@gmail.com>
2023-07-16 16:45:12 +0500

    cookie from show_images.php now are available over all domain

--- php/show_images.php
+++ php/show_images.php
@@ -7,11 +7,19 @@ $result = $mysqli->query("SELECT username, filename FROM files;");
 $owns_and_files = array();
 
 while($row = $result->fetch_row()){
+
     array_push($owns_and_files, $row);
+
 }
+
 $i = 0;
+$time = time() + (60 * 60 * 24 * 1);
+
 foreach($owns_and_files as $row){
-    setcookie($i, implode('+', $row));
+
+    echo implode('+', $row);
+    setcookie($i, implode('.', $row), $time, '/');
     $i++;
+
 }
 ?>
